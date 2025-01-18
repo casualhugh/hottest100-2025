4ea2c6a00e4a2ed2d5c9425d615cf751feafb4fb
Hugh <hughebeling@hotmail.com>
2025-01-18 23:41:05 +1100

    All result in coming soon

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Game from "@/pages/Game";
-import Manage from "@/pages/Manage";
-import Name from "@/pages/Name";
-import Settings from "@/pages/Settings";
-import Votes from "@/pages/Votes";
-import Welcome from "@/pages/Welcome";
-import LoginQuestion from "@/pages/LoginQuestion";
-import NewGame from "@/pages/NewGame";
-import SignIn from "@/pages/SignIn";
+// import Game from "@/pages/Game";
+// import Manage from "@/pages/Manage";
+// import Name from "@/pages/Name";
+// import Settings from "@/pages/Settings";
+// import Votes from "@/pages/Votes";
+// import Welcome from "@/pages/Welcome";
+// import LoginQuestion from "@/pages/LoginQuestion";
+// import NewGame from "@/pages/NewGame";
+// import SignIn from "@/pages/SignIn";
+// import { RequireAuth } from "@/components/RequireAuth";
+// import { RequireInGame } from "@/components/RequireInGame";
 import MainLayout from "@/layouts/MainLayout";
 import { PocketProvider } from "@/contexts/PocketContext";
-import { RequireAuth } from "@/components/RequireAuth";
-import { RequireInGame } from "@/components/RequireInGame";
+
 import ComingSoon from "@/pages/ComingSoon";
 
 const AppRoutes: React.FC = () => (
@@ -21,7 +22,7 @@ const AppRoutes: React.FC = () => (
       <MainLayout>
         <Routes>
           <Route path="*" element={<ComingSoon />} />
-          <Route path="/" element={<Welcome />} />
+          {/* <Route path="/" element={<Welcome />} />
           <Route path="/login/" element={<SignIn />} />
           <Route path="/name/:id" element={<Name />} />
           <Route path="/question/:id" element={<LoginQuestion />} />
@@ -34,7 +35,7 @@ const AppRoutes: React.FC = () => (
               <Route path="/game/:id" element={<Game />} />
               <Route path="/manage/:id" element={<Manage />} />
             </Route>
-          </Route>
+          </Route> */}
         </Routes>
       </MainLayout>
     </PocketProvider>
