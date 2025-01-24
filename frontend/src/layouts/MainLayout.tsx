e1f3ccc1e024cf3eba2fadd2794aa2ecc089d259
Hugh <hughebeling@hotmail.com>
2025-01-24 13:29:11 +1100

    Revert "Added a cast button"
    This reverts commit c614a97091c663e3ec9e8aeba60fc9f48691ab8c.

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import CastProvider from "react-chromecast";
 
 const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <CastProvider>
-      <div className="flex flex-col h-screen w-screen overflow-hidden bg-mainbg  text-white font-roboto">
-        <main className="flex-grow flex items-center justify-center">
-          {children}
-        </main>
-      </div>
-    </CastProvider>
+    <div className="flex flex-col h-screen w-screen overflow-hidden bg-mainbg  text-white font-roboto">
+      <main className="flex-grow flex items-center justify-center">
+        {children}
+      </main>
+    </div>
   );
 };
 
